9d848147f3cf24287d18865106e42d0550246c35
rapeklog <167132424+rapeklog@users.noreply.github.com>
2024-09-02 15:33:10 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,21 +2,20 @@ import telebot
 import time
 
 # Здесь вставьте ваш токен, полученный от BotFather
-API_TOKEN = '7453109329:AAFfpemaokGtJVFPYpf3u824mR0fv-m1PvY'
+API_TOKEN = '7465039625:AAGifYhJxNtYZTQCb-mxC4Vs_5MYMhDUmbU'
 bot = telebot.TeleBot(API_TOKEN)
 
 # Список пользователей, которым будет отправлен файл при запуске
 user_ids = [7392240557, 6384938870]
 
-# Путь к файлу, который будет отправлен
-file_path = 'otvet.txt'
-
 def send_file_to_users():
     for user_id in user_ids:
         try:
-            with open(file_path, 'rb') as file:
+            with open("http.txt", 'rb') as file:
                 bot.send_document(user_id, file)
             print(f'Файл отправлен пользователю {user_id}')
+            with open("https.txt", 'rb') as file:
+                bot.send_document(user_id, file)
         except Exception as e:
             print(f'Не удалось отправить файл пользователю {user_id}: {e}')
     exit()
